4217d8d058b0435a8f230f084c6e060793de3e7b
Lucas Charbit <lucas.charbit@gmail.com>
2019-02-15 08:45:59 -0500

    ---> Initial commit of the Project moved from its original BitBucket repo

---
+++ Source/ArcadeVehicleSystem.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ArcadeVehicleSystemTarget : TargetRules
+{
+	public ArcadeVehicleSystemTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+
+		ExtraModuleNames.AddRange( new string[] { "ArcadeVehicleSystem" } );
+	}
+}
